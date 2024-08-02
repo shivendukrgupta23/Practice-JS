63c680f3468236759ed5273ef4c24a5dcc45de13
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-08-02 08:31:08 +0000

    execution

---
+++ Practice/execute.js
@@ -0,0 +1,8 @@
+//javascript execution context
+//how to execute javascript.
+//Step-1:Global Execution -> this.
+//step-2:Memory  Phase->
+//val1=undefined, val2=undefined,addnum=definition,result1=undefined,result2=undefined.
+//Step-3:Execution Phase->
+//val1=10,val2=5,addnum-> new variable environment + execution thread->1.memory phase->val1=undefined,val2=undefined,total=undefine
+//2.execution context-> num1=10,num2=5,total=15,total jayega Global ke pass and fir new environment wale deleted hojayega.
