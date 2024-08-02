44ff2b124a5eb7afaeeda7ef739217e2f237233c
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-08-02 09:47:10 +0000

    control_flow

---
+++ Practice/control_flow.js
@@ -0,0 +1,98 @@
+//control flow 
+//if statements
+
+// const score=200
+// if(score > 100){
+//     let power="fly";
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power:${power}`);
+
+
+
+
+// const balance=1000
+// if(balance > 500) console.log("test");//implicity scope.
+
+
+// const balance=1000
+// if(balance < 500){
+//     console.log("less than");
+    
+
+// }else if(balance <750){
+//     console.log("less than 750");
+    
+// }else if(balance < 900){
+//     console.log("less than 900");
+    
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+
+
+// const userLoggedIn=true
+// const debitCard=true
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+    
+// }
+
+
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("invalid");
+//         break;
+        
+        
+// }
+
+
+//-----------------------------------------------------
+
+// const userEmail= "Shivaa@gmail.com"
+// if(userEmail){
+//     console.log("Got user email");
+    
+// }else{
+//     console.log("Don't have user email");
+    
+// }
+
+//false
+//false, 0, -0, BigInt, "", null, undefined,NaN
+//Truthly values
+//"0", 'false'," ",[],{},function(){}
+
+
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("Object is empty");
+    
+// }
+
+//Nullish coalescing Operator(??)null ,undefined
+//let val1;
+//val1=5 ?? 10
+//val1=null ?? 10
+//val1=undefined ?? 15
+
+//val1= null ? 10 ?? 15
+
+//console.log(val1);
+
