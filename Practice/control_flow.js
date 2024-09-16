7c46773dc1b46913c9a60e0bcbf335f23d84ba50
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-09-16 14:08:34 +0000

    control_flow

--- Practice/control_flow.js
+++ Practice/control_flow.js
@@ -96,3 +96,14 @@
 
 //console.log(val1);
 
+
+//find number is prime or not:
+
+let val1;
+val1=5 ?? 10;
+val1 = null ?? 10;
+val1 = undefined ?? 15;
+val1 = null ? 10 ?? 15:
+console.log(val1);
+
+
